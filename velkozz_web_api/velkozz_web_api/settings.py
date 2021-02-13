5b8a80feeff258d1b862083e3f125cfaeb2e837f
MatthewTee <teelucksingh.matthew1@gmail.com>
2021-02-13 03:53:55 -0400

    Added frontend elements for main index page

--- velkozz_web_api/velkozz_web_api/settings.py
+++ velkozz_web_api/velkozz_web_api/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 # Adding a global static directory to the app: 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
